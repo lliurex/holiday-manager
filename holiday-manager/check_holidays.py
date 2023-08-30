5eb73c61b82427c7c60b36659d2f93959a68065c
jrpelegrina <juapesai@hotmail.com>
2023-08-30 11:00:52 +0200

    WIP in Migration. Changed to user n4d.client

--- holiday-manager/check_holidays.py
+++ holiday-manager/check_holidays.py
@@ -3,25 +3,21 @@
 import time
 from datetime import date
 
-import xmlrpc.client as n4dclient
-import ssl
-
+import n4d.client
 
 class CheckDay(object):
 
 	def __init__(self):
 
 		self.current_day=date.today().strftime('%d/%m/%Y')
-		context=ssl._create_unverified_context()
-		self.n4d=n4dclient.ServerProxy("https://localhost:9779",context=context,allow_none=True)
+		self.n4d=n4d.client.Client()
 		self.check_day()
 
 	#def __init__	
 
 	def check_day(self):
 
-		#check=self.holidayManager.is_holiday(self.current_day)
-		check=self.n4d.is_holiday('','HolidayListManager',self.current_day)["return"]["status"]
+		check=self.n4d.HolidayListManager.is_holiday(self.current_day)["status"]
 		if check:
 			exit(1) 
 		else:
